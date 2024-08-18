854f4cab28c1c05ee4b4b4ce8cc430d87eebd6ae
AbdulmalekAli60 <ZxshxZ123@gmail.com>
2024-08-18 13:19:19 +0300

    Finish Interface

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,106 @@
+//MUI Library
+import { Divider } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+import Stack from "@mui/material/Stack";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+//MUI Library
+
+//My Components
+import PrayersCards from "./PrayersCards";
+//My Components
+
+export default function MainContent() {
+  const handleChange = (event: SelectChangeEvent) => {
+    console.log(event.target.value);
+  };
+  return (
+    <>
+      {/* Top */}
+      <Grid container>
+        <Grid xs={6}>
+          <div>
+            <h2>4:20 | 9 2023 سبتمر</h2>
+            <h1>مكة المكرمة</h1>
+          </div>
+        </Grid>
+
+        <Grid xs={6}>
+          <div>
+            <h2>متبقي حتى صلاة العصر</h2>
+            <h1>15:45:9</h1>
+          </div>
+        </Grid>
+      </Grid>
+      {/* === Top === */}
+
+      <Divider
+        variant="middle"
+        style={{ borderColor: "white", opacity: "0.1" }}
+      />
+
+      {/* Cards */}
+      <Stack
+        
+        direction="row"
+        justifyContent={"space-around"}
+        style={{ marginTop: "2rem" }}
+      >
+        <PrayersCards
+          prayerNmae="الفجر"
+          time="04:10"
+          image="https://wepik.com/api/image/ai/9a07baa7-b49b-4f6b-99fb-2d2b908800c2"
+        />
+        <PrayersCards
+          prayerNmae="الظهر"
+          time="12:23"
+          image="https://wepik.com/api/image/ai/9a07bb45-6a42-4145-b6aa-2470408a2921"
+        />
+        <PrayersCards
+          prayerNmae="العصر"
+          time="03:28"
+          image="https://wepik.com/api/image/ai/9a07bb90-1edc-410f-a29a-d260a7751acf"
+        />
+        <PrayersCards
+          prayerNmae="المغرب"
+          time="06:54"
+          image="https://wepik.com/api/image/ai/9a07bbe3-4dd1-43b4-942e-1b2597d4e1b5"
+        />
+        <PrayersCards
+          prayerNmae="العشاء"
+          time="08:07"
+          image="https://wepik.com/api/image/ai/9a07bc25-1200-4873-8743-1c370e9eff4d"
+        />
+      </Stack>
+      {/* Cards */}
+
+      {/* Select */}
+      <Stack direction="row" justifyContent={"center"}>
+        <FormControl style={{ width: "30%", marginTop: "2rem" }}>
+          <InputLabel id="demo-simple-select-label">
+            <span style={{ color: "white" }}>المدينة</span>
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            // value={age}
+            label="Age"
+            onChange={handleChange}
+          >
+            <MenuItem value={10}>الرياض</MenuItem>
+            <MenuItem value={20}>بريدة</MenuItem>
+            <MenuItem value={30}>جدة</MenuItem>
+          </Select>
+        </FormControl>
+      </Stack>
+      {/* Select */}
+    </>
+  );
+}
+//fjr https://wepik.com/api/image/ai/9a07baa7-b49b-4f6b-99fb-2d2b908800c2
+// dhr https://wepik.com/api/image/ai/9a07bb45-6a42-4145-b6aa-2470408a2921
+// asr https://wepik.com/api/image/ai/9a07bb90-1edc-410f-a29a-d260a7751acf
+// mgrb https://wepik.com/api/image/ai/9a07bbe3-4dd1-43b4-942e-1b2597d4e1b5
+// isha https://wepik.com/api/image/ai/9a07bc25-1200-4873-8743-1c370e9eff4d
---
+++ src/components/PrayersCards.tsx
@@ -0,0 +1,27 @@
+//MUI Library
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+//MUI Library
+
+interface Prayer {
+  prayerNmae: string;
+  time: string;
+  image: string;
+}
+
+export default function PrayersCards({ prayerNmae, time, image }: Prayer) {
+  return (
+    <>
+      <Card sx={{ width: "15vw" }}>
+        <CardMedia sx={{ height: 140 }} image={image} />
+        <CardContent>
+          <h2>{prayerNmae}</h2>
+          <Typography color="text.secondary" variant="h3">{time}</Typography>
+        </CardContent>
+      </Card>
+    </>
+  );
+}
